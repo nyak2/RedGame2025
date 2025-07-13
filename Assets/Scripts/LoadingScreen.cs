1ea455f3cc98298206937607bd8fb4e6f7fe70fc
AikChun <lac@chaosnut.com>
2025-07-13 11:54:05 +0800

    Make main menu ui look better with tweening and other stuff

---
+++ Assets/Scripts/LoadingScreen.cs
@@ -0,0 +1,58 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingScreen : MonoBehaviour
+{
+    [SerializeField] private Image _loadImage;
+    [SerializeField] private List<Sprite> _sprites;
+    [SerializeField] private Image _loadBgImage;
+
+    private Sequence _loadingSequence;
+    private Sequence _rotateSequence;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _loadImage.sprite = _sprites[Random.Range(0, _sprites.Count)];
+        _loadImage.DOColor(new Color(_loadImage.color.r, _loadImage.color.g, _loadImage.color.b, 0), 0.0f);
+        _loadBgImage.DOColor(new Color(_loadBgImage.color.r, _loadBgImage.color.g, _loadBgImage.color.b, 0), 0.0f);
+        _loadBgImage.raycastTarget = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+           
+    }
+
+    public void EnableLoadingScreen()
+    {
+        _loadingSequence = DOTween.Sequence();
+            
+        _loadingSequence.Append(_loadBgImage.DOColor(new Color(_loadBgImage.color.r, _loadBgImage.color.g, _loadBgImage.color.b, 1.0f), 0.25f));
+        _loadingSequence.Join(_loadImage.DOColor(new Color(_loadImage.color.r, _loadImage.color.g, _loadImage.color.b, 1.0f), 0.25f)).SetDelay(0.15f);
+        _loadBgImage.raycastTarget = true;
+
+        _rotateSequence = DOTween.Sequence();
+
+        _rotateSequence.Append(_loadImage.transform
+            .DOLocalRotate(new Vector3(0, 0, 20), 0.8f)
+            .SetEase(Ease.InOutSine)).SetLoops(-1, LoopType.Yoyo);
+    }
+
+    public Sequence FadeLoadingBgIamge()
+    {
+        _loadingSequence = DOTween.Sequence();
+
+        _loadingSequence.Append(_loadImage.DOColor(new Color(_loadImage.color.r, _loadImage.color.g, _loadImage.color.b, 0.0f), 0.25f));
+
+        return _loadingSequence;
+    }
+
+    void TweenLoadImage()
+    {
+
+    }
+}
--- Assets/Scripts/MainMeneUIManager.cs
+++ Assets/Scripts/MainMeneUIManager.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,9 +6,43 @@ using UnityEngine.SceneManagement;
 
 public class MainMeneUIManager : MonoBehaviour
 {
-    [SerializeField] string _gameScenename;
+    [SerializeField] string _gameSceneName;
+    [SerializeField] LoadingScreen _loadingScreen;
+    [SerializeField] LeaderBoardManager _leaderBoardManager;
+
+    private Sequence _loadGameSequence;
+
+    private void Awake()
+    {
+
+    }
     public void PlayGame()
     {
-        SceneManager.LoadScene(_gameScenename);
+        StartCoroutine(LoadGameScene());
+    }
+
+    public void ShowLeaderBoarad()
+    {
+        _leaderBoardManager.ShowLeaderBoard();
+    }
+
+    public void HideLeaderBoarad()
+    {
+        _leaderBoardManager.HideLeaderBoard(0.25f);
+    }
+
+    public IEnumerator LoadGameScene()
+    {
+        _loadingScreen.EnableLoadingScreen();
+        yield return new WaitForSeconds(1.5f);
+
+        _loadGameSequence = DOTween.Sequence();
+
+        _loadGameSequence.Append(_loadingScreen.FadeLoadingBgIamge());
+
+        _loadGameSequence.OnComplete(() =>
+        {
+            SceneManager.LoadScene(_gameSceneName);
+        });
     }
 }
